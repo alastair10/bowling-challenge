db5297df939aeaf4ce20a96e90e701b8965ec20c
Alastair <alastair.chau@gmail.com>
2022-11-18 21:50:58 +0000

    runBonus function testing in prog

--- score.js
+++ score.js
@@ -3,26 +3,51 @@ class Score {
   constructor () {
     this.r1 = 0;
     this.r2 = 0;
+    this.b1 = 0;
+    this.b2 = 0;
     this.framescore = 0;
-    this.scoreList = new Map();
     this.currentRoll = 0;
+    this.scoreList = new Map();
   }
 
-  // sets up the frame values (roll 1, roll 2, and map)
-  runFrame(r1, r2) {
+  // sets up the roll scores)
+  runRoll(r1, r2) {
     this.r1 = r1;
     this.r2 = r2;
-    this.currentRoll += 1;
-    this.scoreList.set(`r${this.currentRoll}`,this.r1);
-    this.currentRoll += 1;
-    this.scoreList.set(`r${this.currentRoll}`,this.r2);
+    this.currentRoll += 2;
+    this.scoreList.set(`r${this.currentRoll-1}`,this.r1)
+      .set(`r${this.currentRoll}`,this.r2);
   }
 
   // determines the frame score
-  frameScore() {
+  getFrameScore() {
     this.framescore = this.r1 + this.r2;
     return this.framescore;
   }
+
+  // assigns bonus scores
+  runBonus () {
+    // this frame < 10, set bonuses to 0
+    if (this.framescore < 10) {
+      this.scoreList.set(`b${this.currentRoll-1}`,0)
+        .set(`b${this.currentRoll}`,0);
+
+    // previous frame = spare, add currentRoll 1 to previous frame bonus 1
+    } else if ((this.scoreList.get(`r${this.currentRoll-3}`) + (this.scoreList.get(`r${this.currentRoll-2}`))) === 10) {
+      this.scoreList.set(`b${this.currentRoll-3}`, this.r1)
+        .set(`b${this.currentRoll-2}`,0);
+
+    // prior two strikes in a row
+    } else if ((this.scoreList.get(`r${this.currentRoll-5}`) === 10) && (this.scoreList.get(`r${this.currentRoll-3}`) === 10)) {
+      this.scoreList.set(`b${this.currentRoll-5}`, 10)
+        .set(`b${this.currentRoll-4}`, 10);
+    
+    // previous frame = strike and this frame != strike, add both rolls this frame to previous frame bonuses
+    } else if (this.scoreList.get(`r${this.currentRoll-3}`) === 10) {
+      this.scoreList.set(`b${this.currentRoll-3}`, this.r1)
+        .set(`b${this.currentRoll-2}`, this.r2);
+    }
+  };
 
   // determines whether a user has hit a strike or spare
   strikeOrSpare() {
@@ -32,6 +57,7 @@ class Score {
       return 'spare';
     };
   }
+
 
 }
 
--- score.test.js
+++ score.test.js
@@ -1,42 +1,72 @@
 const Score = require('./score')
 
-
-describe ('runFrame', () => {
-  it ('adds user scores to the scoreList map', () => {
+describe ('runBonus', () => {
+  it ('assigns 0 bonus for current frame', () => {
     let score = new Score();
-    score.runFrame(9,1);
-    expect(score.scoreList.get('r1')).toEqual(9);
-    expect(score.scoreList.get('r2')).toEqual(1);
+    score.runRoll(3,4);
+    score.getFrameScore();
+    score.runBonus();
+    expect(score.scoreList.get('b1')).toEqual(0);
+    expect(score.scoreList.get('b2')).toEqual(0);
+  });
+
+  it ('assigns two bonuses for a spare from previous round', () => {
+    let score = new Score();
+    score.runRoll(5,5); //spare
+    score.getFrameScore();
+    score.runBonus();
+    score.runRoll(2,1);
+    score.getFrameScore();
+    score.runBonus();
+    expect(score.scoreList.get('b1')).toEqual(2);
+    expect(score.scoreList.get('b2')).toEqual(0);
   });
 });
 
-describe ('frameScore', () => {
+describe ('runRoll', () => {
+  it ('adds user scores to the scoreList map', () => {
+    let score = new Score();
+    score.runRoll(9,1);
+    expect(score.scoreList.get('r1')).toEqual(9);
+    expect(score.scoreList.get('r2')).toEqual(1);
+  });
+
+  it ('adds user scores to the scoreList map after 2 rounds', () => {
+    let score = new Score();
+    score.runRoll(1,2);
+    score.runRoll(3,4);
+    expect(score.scoreList.get('r3')).toEqual(3);
+    expect(score.scoreList.get('r4')).toEqual(4);
+  });
+});
+
+describe ('getFrameScore', () => {
   it ('returns the sum of 2 rolls as a frame score', () => {
     let score = new Score();
-    score.runFrame(2,5);
-    expect(score.frameScore()).toEqual(7)
+    score.runRoll(2,5);
+    expect(score.getFrameScore()).toEqual(7)
   });
 });
 
 describe ('strikeOrSpare', () => {
   it ('says user rolled a strike when first roll in frame = 10', () => {
     let score = new Score();
-    score.runFrame(10,0);
-    score.frameScore();
+    score.runRoll(10,0);
+    score.getFrameScore();
     expect(score.strikeOrSpare()).toEqual('strike');
   });
 
   it ('says user rolled a spare when framescore = 10', () => {
     let score = new Score();
-    score.runFrame(4,6);
-    score.frameScore();
+    score.runRoll(4,6);
+    score.getFrameScore();
     expect(score.strikeOrSpare()).toEqual('spare');
   });
 
   it ('does not return anything if neither strike nor spare', () => {
     let score = new Score();
-    score.runFrame(2,3);
-    score.frameScore();
+    score.runRoll(2,3);
+    score.getFrameScore();
     expect(score.strikeOrSpare()).not.toEqual('strike');
     expect(score.strikeOrSpare()).not.toEqual('spare');
     expect(score.strikeOrSpare()).toEqual(undefined)
